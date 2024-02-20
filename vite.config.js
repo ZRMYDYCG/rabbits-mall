0d2a6587310eda979f29da5c8d081fa19a8265e4
ZRMYDYCG <547471919@qq.com>
2024-02-20 14:09:29 +0800

    Element Plus组件自动按需导入

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// ElmentPlus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
   ],
   resolve: {
+    // 实际的路劲转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
