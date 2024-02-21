12b97bbb0dd70f19cc3c58e8e1bf325f81e7f00f
ZRMYDYCG <547471919@qq.com>
2024-02-21 12:14:59 +0800

    Home-Banner轮播数据获取渲染

--- src/api/index.js
+++ src/api/index.js
@@ -16,7 +16,7 @@ httpInstance.interceptors.response.use(res => res.data, e => {
 return Promise.reject(e)
 })
 
-/*****首页页面*****/
+// TODO: Layout 模块
 /**
  * @desc 获取全部分类(包含推荐商品) 注意: 可以认为获取的是一级分类列表
  * @param 无
@@ -27,5 +27,19 @@ export const getCategoryAPI = () => {
   return httpInstance({
     url: "/home/category/head",
     method: 'get'
+  })
+}
+
+// TODO: Home 模块
+/**
+ * @desc 获取轮播图数据
+ * @param 无
+ * @data 无
+ * @return Object
+ */
+
+export const getBannerAPI = () => {
+  return httpInstance({
+    url: '/home/banner'
   })
 }
--- src/views/Home/components/HomeBanner/index.vue
+++ src/views/Home/components/HomeBanner/index.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 /**
  * 责任人：Array
  * ['曾日闽']
@@ -7,21 +7,51 @@
 /***
  * 组件依赖
  * **/
-
+import { getBannerAPI } from '@/api/index.js'
+import { onMounted, ref } from 'vue'
 
 /***
  * 脚本主体逻辑
  * **/
+// TODO: 获取 Banner 数据，进行动态渲染
+const bannerList = ref([])
+const getBanner = async () => {
+  const { result } = await getBannerAPI()
+  bannerList.value = result
+}
 
 /***
  * 声明周期钩子
  * **/
+onMounted(() => {
+  getBanner()
+})
 </script>
 
 <template>
-    产品Banner
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<style scoped lang="scss">
-    
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 </style>
